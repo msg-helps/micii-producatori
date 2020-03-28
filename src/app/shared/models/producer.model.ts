36dbaaf6d9eb1a1a464780bfc10e4bc7ce7e043c
Akysh5 <akysh.baymuhammedov@msg.group>
2020-03-28 17:05:48 +0200

    setup test commit

---
+++ src/app/shared/models/producer.model.ts
@@ -0,0 +1,27 @@
+
+/**
+ *
+ * An interface representing Producer model.
+ */
+export interface Producer {
+  /**
+   * @member {string} [id]
+   */
+  id: string;
+  /**
+   * @member {string} [firstName]
+   */
+  firstName: string;
+  /**
+   * @member {string} [lastName]
+   */
+  lastName: string;
+  /**
+   * @member {string} [phone]
+   */
+  phone: string;
+  /**
+   * @member {string} [email]
+   */
+  email: string;
+}
