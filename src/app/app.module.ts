b13bdc6d3190c20e6bf8553c79c96d8fe2fde017
Alex Luchian <>
2020-03-28 19:02:36 +0200

    Add material and basic app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterModule } from "./modules/register/register.module";
+import { UserModule } from "./modules/user/user.module";
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RegisterModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/models/consumer.model.ts
@@ -0,0 +1,23 @@
+
+/**
+ *
+ * An interface representing Consumer model.
+ */
+export interface Consumer {
+  /**
+   * @member {string} [id]
+   */
+  id: string;
+  /**
+   * @member {string} [firstName]
+   */
+  firstName: string;
+  /**
+   * @member {string} [lastName]
+   */
+  lastName: string;
+  /**
+   * @member {string} [phone]
+   */
+  phone: string;
+}
