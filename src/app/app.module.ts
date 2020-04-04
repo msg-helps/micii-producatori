d4ad2792dd431531c76aebcc5d64b092f27b133e
Akysh5 <akysh.baymuhammedov@msg.group>
2020-04-04 14:09:50 +0300

    Login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { RegisterModule } from './modules/register/register.module'
-import { UserModule } from './modules/user/user.module'
-import { MainModule } from './modules/main/main.module'
-import { ProductsModule } from './modules/products/products.module'
-import { HttpClientModule } from '@angular/common/http'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterModule } from './modules/register/register.module';
+import { UserModule } from './modules/user/user.module';
+import { MainModule } from './modules/main/main.module';
+import { ProductsModule } from './modules/products/products.module';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthModule } from './modules/auth/auth.module';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
   declarations: [AppComponent],
@@ -21,6 +23,8 @@ import { HttpClientModule } from '@angular/common/http'
     UserModule,
     MainModule,
     ProductsModule,
+    AuthModule,
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/modules/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './components/login/login.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+
+
+@NgModule({
+  declarations: [LoginComponent, ResetPasswordComponent],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ],
+  exports: [
+    LoginComponent,
+    ResetPasswordComponent
+  ]
+})
+export class AuthModule { }
